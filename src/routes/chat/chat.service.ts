c25a8e4ba6624cfbb39d5a5fc9eaf727c8ea1572
gabriel <gabrieljoseDev77@outlook.com>
2023-08-26 15:52:25 -0300

    feat(chat.service) adicionando uma mensagem quando o usuario encerrar a conexao

--- src/routes/chat/chat.service.ts
+++ src/routes/chat/chat.service.ts
@@ -5,17 +5,11 @@ import { Observable, Subject } from "rxjs";
 import { IChat } from "src/common/interfaces/chat.interface";
 import { ChatServiceDomain } from "src/domain/chat/chat.service.domain";
 
-interface AttempsMsg{
-    toUser: IChat.User
-    messagePayload: IChat.ReceiveMsg
-}
-
-
 @Injectable()
 export class ChatService implements ChatServiceDomain{
 
     private readonly users: IChat.User[] = []
-    private readonly attempsMsg: AttempsMsg[] = []
+    private readonly attempsMsg: IChat.AttempsMsg[] = []
 
     createUser(payload: IChat.CreateUser, call: ServerUnaryCall<IChat.CreateUser, IChat.ResponseCreateUser>){
         const userExists = this.users.findIndex(v => v.user === payload?.user) === -1
@@ -29,12 +23,12 @@ export class ChatService implements ChatServiceDomain{
             return {
                 error: "",
                 userCreated: true
-            }
+            } as IChat.ResponseCreateUser
         }
         return {
             error: userExists && payload.user? "User already exists" :"User not created",
             userCreated: false
-        }
+        } as IChat.ResponseCreateUser
     }
 
     getAllUsers(): IChat.ResponseGetAll{
@@ -45,25 +39,28 @@ export class ChatService implements ChatServiceDomain{
     }
 
     joinChat(payload: Observable<IChat.SendToUser>): Observable<IChat.ReceiveMsg>{
-        const subject = new Subject<IChat.ReceiveMsg>();
+        const subjectChat = new Subject<IChat.ReceiveMsg>();
+        let fromUser: IChat.User;
+        let userTo: IChat.User;
         payload.subscribe({
             next: (payload) => {
-                const fromUser = this.users.find(v => v.user === payload.from)
-                if(this.users.length === 0 || !fromUser) return subject.error(new RpcException(!fromUser? "User entered does not exist" :"No users"))
-                if(!fromUser.subject) fromUser.subject = subject
+                fromUser = this.users.find(v => v.user === payload.from)
+                if(this.users.length === 0 || !fromUser) return subjectChat.error(new RpcException(!fromUser? "User entered does not exist" :"No users"))
+                if(!fromUser.subject) fromUser.subject = subjectChat
                 if(this.attempsMsg.length !== 0){
-                    this.attempsMsg.forEach(msg => {
+                    this.attempsMsg.forEach((msg, index) => {
                         const messagePayload: IChat.ReceiveMsg = {
                             from: msg.messagePayload.from,
                             msg: msg.messagePayload.msg
                         }
                         msg.toUser.subject.next(messagePayload)
+                        this.attempsMsg.splice(index, 1)
                     })
                 }
-                const userTo = this.users.find(v => v.id === payload.toID)
+                userTo = this.users.find(v => v.id === payload.toID)
                 if(!userTo || userTo.id === fromUser.id && userTo.user === fromUser.user) {
-                    subject.error(new RpcException(!userTo? "User to send message not exists" :"It is not possible to send a message to yourself"))
-                    return subject.complete()
+                    subjectChat.error(new RpcException(!userTo? "User to send message not exists" :"It is not possible to send a message to yourself"))
+                    return subjectChat.complete()
                 }
                 const messagePayload: IChat.ReceiveMsg = {
                     from: payload.from,
@@ -75,16 +72,23 @@ export class ChatService implements ChatServiceDomain{
                         messagePayload: messagePayload,
                         toUser: userTo
                     })
-                    return subject.next({
+                    return subjectChat.next({
                         from: "Server",
                         msg: "Waiting for user to connect!"
                     })
                 }
             },
             complete: () => {
-                subject.complete();
+                delete fromUser.subject
+                if(userTo.subject) userTo.subject.next(
+                    {
+                        from: "Server",
+                        msg: `${userTo.user} disconnected!`
+                    }
+                )
+                subjectChat.complete();
             },
         });
-        return subject.asObservable();
+        return subjectChat.asObservable();
     }
 }
