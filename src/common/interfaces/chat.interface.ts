c2af17879bd7dc1b38db1f6640d9d18330711305
gabriel <gabrieljoseDev77@outlook.com>
2023-08-26 14:12:42 -0300

    doc(interface) documentando as interfaces

--- src/common/interfaces/chat.interface.ts
+++ src/common/interfaces/chat.interface.ts
@@ -12,32 +12,79 @@ export namespace IChat{
     // Public
     export type OmitContextConnectionInGetUser = Omit<User, "contextConnection">
 
+    /**
+     * User created, enter your name
+     */
     export type CreateUser = {
         user: string
     }
 
+    /**
+     * User information
+     */
     export interface User extends CreateUser{
+        /**
+        * Identifier
+        */
         id: number,
+
+        /**
+         * Connection information
+         */
         contextConnection: ServerUnaryCall<any, any>,
+
+        /**
+         * Multicast from Observable
+         */
         subject?: MySubjectNextFuction
     }
 
+    /**
+     * Information to send message
+     */
     export interface SendToUser{
+        /**
+         * Name of the user who will send the message
+         */
         from: string
+
+        /**
+         * Id that receive message
+         */
         toID: number
+
+        /**
+         * Message
+         */
         msg: string
     }
 
     // Response
 
+    /**
+     * User creation answer
+     */
     export type ResponseCreateUser = {
+        /**
+         * Msg of error
+         */
         error: string
+
+        /**
+         * If true, the user was created successfully
+         */
         userCreated: boolean
     }
 
+    /**
+     * Return all users created
+     */
     export interface ResponseGetAll{
         users: OmitContextConnectionInGetUser[]
     }
 
+    /**
+     * Return message information
+     */
     export interface ReceiveMsg extends OmitIdInSendMsg{}
 }
